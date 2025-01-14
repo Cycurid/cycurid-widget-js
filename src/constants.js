f4e5646d749ba4d2ceea11224beefe6093bbef67
Jordan <developer@cycurid.com>
2025-01-14 13:34:22 -0800

    updated constants and version

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
-const OAUTH_SERVER = "https://api2.cycurid.com";
-const CYCURIDWIDGET_URL = "https://connect.cycurid.com";
+const OAUTH_SERVER = 'https://oauth.cycurid.com';
+const CYCURIDWIDGET_URL = 'https://connect.cycurid.com';
+
+// const OAUTH_SERVER = 'http://localhost:3000';
+// const CYCURIDWIDGET_URL = 'http://localhost:3004';
 
 exports.CYCURIDWIDGET_URL = CYCURIDWIDGET_URL;
 exports.OAUTH_SERVER = OAUTH_SERVER;
